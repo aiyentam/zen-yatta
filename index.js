b466780d15a7d84cba77b27d03301a73e4306e4f
Ai Yen Tam <tam.aiyen@gmail.com>
2018-04-16 20:17:20 -0400

    made a fish

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var fish = 0;
-
 // happens once
 function setup() {
   createCanvas(800, 600);
@@ -14,16 +12,85 @@ function draw() {
   // cone(40, 70);
 
   // fish = fish + 1;
-  flowerOne();
+  fish();
 }
 
 // event
 function mousePressed() {}
 
+function fish() {
+  fill(252, 152, 90);
+  noStroke();
+
+  //head
+  push();
+  translate(250, 255);
+  beginShape();
+
+  curveVertex(0, 0);
+  curveVertex(1.25, 0);
+  curveVertex(3.05, 0.49);
+  curveVertex(4.71, 2.54);
+  curveVertex(13.14, 22.26);
+  curveVertex(4.5, 30.17);
+  curveVertex(-4.5, 30.17);
+  curveVertex(-13.14, 22.26);
+  curveVertex(-4.71, 2.54);
+  curveVertex(-3.05, 0.49);
+  curveVertex(-1.25, 0);
+  curveVertex(0, 0);
+
+  endShape();
+  pop();
+
+  //eyes
+  push();
+  translate(250, 275);
+  fill(0, 0, 0);
+  ellipse(5, -2, 5, 5);
+  ellipse(-5, -2, -5, -5);
+  pop();
+
+  //body
+  push();
+  translate(250, 268);
+  noStroke();
+
+  ellipse(0, 38, 23, 50);
+  pop();
+
+  //tail fin
+  push();
+  translate(250, 195);
+
+  beginShape();
+
+  curveVertex(0, 125);
+  curveVertex(0.5, 120.0);
+  curveVertex(4.95, 150.39);
+  curveVertex(0.56, 144.03);
+  curveVertex(0, 143.45);
+  curveVertex(-0.56, 144.03);
+  curveVertex(-4.95, 150.39);
+  curveVertex(-0.5, 120.0);
+  curveVertex(0, 125);
+
+  endShape();
+  pop();
+
+  //fins
+  push();
+  translate(250, 255);
+  triangle(10, 30, 30, 34, 20, 45);
+
+  triangle(-10, 30, -30, 34, -20, 45);
+
+  pop();
+}
+
 function flowerOne() {
   // flower base
-  translate(mouseX, mouseY); // change to mouseX, mouseY
-  noStroke();
+  translate(mouseX, mouseY);
   fill(177, 214, 185);
   for (var i = 0; i < 10; i++) {
     ellipse(0, 10, 20, 60);
